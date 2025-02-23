b22a5dac1bfab99cecb5c62d54c5751e097938a2
Huy <nqht123456789@gmail.com>
2025-02-23 18:57:37 +0700

    add cross origin

--- SportifyService/src/main/java/com/sportify/service/controller/BookingController.java
+++ SportifyService/src/main/java/com/sportify/service/controller/BookingController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +20,7 @@ import com.sportify.service.dtos.booking.BookingDTO;
 import com.sportify.service.dtos.booking.CreateBookingDTO;
 import com.sportify.service.services.BookingService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/bookings")
 public class BookingController {
