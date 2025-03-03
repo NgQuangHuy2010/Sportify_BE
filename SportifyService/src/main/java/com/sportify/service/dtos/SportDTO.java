17ab1466e796a91ea73e2a20ca0ca167703cdb3e
VietVu <vietvu1398@gmail.com>
2025-03-03 20:20:44 +0700

    update dto

---
+++ SportifyService/src/main/java/com/sportify/service/dtos/SportDTO.java
@@ -0,0 +1,20 @@
+package com.sportify.service.dtos;
+
+import com.sportify.service.dtos.admin.sport.SportList;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class SportDTO {
+	private Long id;
+	private String sportName;
+	private String imageUrl;
+	
+	public SportDTO(Long id, String sportName, String image) {
+        this.id = id;
+        this.sportName = sportName;
+        this.imageUrl = image; // Đường dẫn đầy đủ
+    }
+}
